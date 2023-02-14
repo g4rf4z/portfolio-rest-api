14b9690702e50972c28c68484567c81dd52ec381
g4rf4z <gregoire.pinte@gmail.com>
2023-02-14 11:22:38 +0100

    Set file size limit

--- src/server/createServer.ts
+++ src/server/createServer.ts
@@ -16,7 +16,7 @@ const createServer = () => {
       methods: ["GET", "POST", "PATCH", "DELETE"],
     })
   );
-  app.use(express.json());
+  app.use(express.json({ limit: "5MB" }));
   app.use(cookieParser());
   app.use(deserializeToken);
 
